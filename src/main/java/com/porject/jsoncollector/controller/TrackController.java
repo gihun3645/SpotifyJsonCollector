9f5a63fa971e0bb1b20b5d369e1b88d848391b17
gihun3645 <gihun3645@naver.com>
2023-03-24 23:17:15 +0900

    update - 트랙 db 전체 조회, 삭제

--- src/main/java/com/porject/jsoncollector/controller/TrackController.java
+++ src/main/java/com/porject/jsoncollector/controller/TrackController.java
@@ -1,21 +1,37 @@
 package com.porject.jsoncollector.controller;
 
+import com.porject.jsoncollector.model.Track;
 import com.porject.jsoncollector.service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TrackController {
     @Autowired
     private TrackService trackService;
 
+    // 포스트맨으로 Json 데이터 입력하기
     @PostMapping("/tracks/save")
     public ResponseEntity<String> saveTracksFromJson(@RequestBody String json) {
         trackService.saveTracksFromJson(json);
-        return ResponseEntity.ok("Tracks saved successfully");
+        return ResponseEntity.ok("Track 라이브러리가 성공적으로 저장되었습니다.");
+    }
+
+    // 라이브러리 자료 전부 리턴
+    @GetMapping("/tracks")
+    public ResponseEntity<List<Track>> getAllTracks() {
+        List<Track> tracks = trackService.findAllTracks();
+        return ResponseEntity.ok(tracks);
+    }
+
+    // 트랙 전체 삭제
+    @DeleteMapping("/tracks")
+    public ResponseEntity<String> deleteAllTracks() {
+        trackService.deleteAllTracks();
+        return ResponseEntity.ok("모든 트랙이 성공적으로 삭제되었습니다.");
     }
 }
 
--- src/main/java/com/porject/jsoncollector/service/TrackService.java
+++ src/main/java/com/porject/jsoncollector/service/TrackService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import utills.JsonUtil;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,6 +15,7 @@ public class TrackService {
     @Autowired
     private TrackRepository trackRepository;
 
+    // Json data를 sql에 저장
     public void saveTracksFromJson(String json) {
         try {
             List<Track> tracks = JsonUtil.readJson(json);
@@ -21,5 +23,20 @@ public class TrackService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    // Track DB 전체 조회
+    public List<Track> findAllTracks() {
+        try {
+            return trackRepository.findAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>(); // 예외처리로 빈 리스트를 반환
+        }
+    }
+
+    // Track DB 전체 삭제
+    public void deleteAllTracks() {
+        trackRepository.deleteAll();
     }
 }
